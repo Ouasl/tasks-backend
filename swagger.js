05da13a9eccc29e0719be3fcfc9cb6d825859354
Soufiane Hassani <soufiane.hassani@ouasl.com>
2025-04-09 10:07:16 +0100

    change images iurl

--- swagger.js
+++ swagger.js
@@ -3,7 +3,8 @@ document.title = "Wibitech Tasks API Docs"; // Change the document title
 const favIcons = document.querySelectorAll("link[rel~='icon']");
 favIcons.forEach((link) => {
   if (link.href.includes("favicon")) {
-    link.href = "/img/favicon.png"; // Change the favicon
+    link.href =
+      "https://cdn.jsdelivr.net/gh/Ouasl/tasks-backend@90a78f726821ee68433b73833e2cbd7a7453e366/img/favicon.png"; // Change the favicon
   }
 });
 
@@ -23,11 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
       console.log("topbar: ", topBar);
       if (topBar) {
         clearInterval(interval);
-        topBar.innerHTML = `<img src="/img/wibitech-logo-w.svg" />`;
+        topBar.innerHTML = `<img src="https://cdn.jsdelivr.net/gh/Ouasl/tasks-backend@90a78f726821ee68433b73833e2cbd7a7453e366/img/wibitech-logo-w.svg" />`;
       }
     }, 50);
-
-  //   if (topbar) {
-  //     topbar.textContent = `<img src="/img/wibitech-logo.svg" />`;
-  //   }
 });
